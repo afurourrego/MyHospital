39e1c0a79fcd4848415f313f90ea4d92818ea8d3
afurourrego <infoafuro@gmail.com>
2019-08-30 15:32:01 -0500

    work in application js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,19 +15,19 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-// //= require jquery
-// //= require rails-ujs
-// //= require activestorage
-// //= require turbolinks
-// //= require materialize
-// //= require_tree .
 //
-// document.addEventListener('turbolinks:load', function() {
-//     M.updateTextFields();
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require materialize
+//= require_tree .
+
+
+document.addEventListener('turbolinks:load', function() {
+  M.updateTextFields();
 //
 //     slidOutMenu                 = document.querySelector('#slide-out');
-//     actionBtn                   = document.querySelector('.fixed-action-btn');
 //     collapsible                 = document.querySelectorAll('.collapsible');
 //     datePikers                  = document.querySelectorAll('.datepicker-date');
 //     timePikers                  = document.querySelectorAll('.datepicker-time');
@@ -35,25 +35,22 @@ require("channels")
 //     $select                     = $('select')
 //
 //     slidOutMenuInstance         = new M.Sidenav(slidOutMenu, {});
-//     actionBtnInstance           = new M.FloatingActionButton(actionBtn, { direction: 'left' });
 //     collapsibleInstance         = M.Collapsible.init(collapsible, {});
 //     datePickerinstances         = M.Datepicker.init(datePikers, { showClearBtn: true, autoClose: true, yearRange: 80 });
 //     timePickerinstances         = M.Timepicker.init(timePikers, { showClearBtn: true, autoClose: true });
 //
 //
 //     if ($select.length > 0 )                $('select').select2();
-//   });
-//
-// document.addEventListener('turbolinks:before-visit', function() {
+  });
+
+document.addEventListener('turbolinks:before-visit', function() {
 //     slidOutMenu                 = document.querySelector('#slide-out');
-//     actionBtn                   = document.querySelector('.fixed-action-btn');
 //     collapsible                 = document.querySelectorAll('.collapsible');
 //     datePikers                  = document.querySelectorAll('.datepicker-date');
 //     timePikers                  = document.querySelectorAll('.datepicker-time');
 //
 //
 //     slidOutMenuInstance         = M.Sidenav.getInstance(slidOutMenu);
-//     actionBtnInstance           = M.FloatingActionButton.getInstance(actionBtn);
 //
 //     if (slidOutMenuInstance)    slidOutMenuInstance.destroy();
 //     if (actionBtnInstance)      actionBtnInstance.destroy();
@@ -79,10 +76,10 @@ require("channels")
 //       });
 //     };
 //
-//   });
-//
-// document.addEventListener('turbolinks:before-cache', function() {
+  });
+
+document.addEventListener('turbolinks:before-cache', function() {
 //   if ($select.length > 0) $('select').each(function() {
 //     $(this).select2('destroy');
 //   });
-// });
+});

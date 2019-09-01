044160167fadcaf12004f7cd95b2658078d3715e
afurourrego <infoafuro@gmail.com>
2019-09-01 00:15:58 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get 'welcome/index'
-
-  root to: 'welcome#index'
+  # get 'welcome/index'
+  #
+  # root to: 'welcome#index'
+  root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

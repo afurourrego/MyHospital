294923b6ee1144739a18960123d7fca50ae90d8a
afurourrego <infoafuro@gmail.com>
2019-09-10 22:56:58 -0500

    database cleanner

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,27 +4,21 @@ RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
   before do
-    binding.pry
-    user1 = user
+    user
     # @user1 = create(:user)
   end
 
   it "is valid with valid attributes" do
-    expect(user1).to be_valid
+    expect(user).to be_valid
   end
 
   it "has a unique email" do
-    user2 = build(:user, name: "Bob")
+    user2 = build(:user, email: user.email)
     expect(user2).to_not be_valid
   end
 
   it "is not valid without a password" do
     user2 = build(:user, password: nil)
-    expect(user2).to_not be_valid
-  end
-
-  it "is not valid without a username" do
-    user2 = build(:user, name: nil)
     expect(user2).to_not be_valid
   end
 

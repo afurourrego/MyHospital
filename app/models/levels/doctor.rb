10fbbf6a1479a9168db1013a772d0732cddf2964
afurourrego <infoafuro@gmail.com>
2019-09-11 23:07:01 -0500

    identacion

--- app/models/levels/doctor.rb
+++ app/models/levels/doctor.rb
@@ -2,6 +2,7 @@ module Levels
   module Doctor
     def doctor_rules
       can %i[index read update],                                                User, id: @user.id
-      cannot %i[create destroy],                                                User    end
+      cannot %i[create destroy],                                                User
+    end
   end
 end

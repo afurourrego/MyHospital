83da4d508d9edb990d3996ec3a8e0e247fa03943
afurourrego <infoafuro@gmail.com>
2019-09-12 22:50:48 -0500

    rules for patients

--- app/models/levels/administrator.rb
+++ app/models/levels/administrator.rb
@@ -3,6 +3,9 @@ module Levels
     def administrator_rules
       can %i[index read update],                                                User, id: @user.id
       cannot %i[create destroy],                                                User
+
+      can %i[index read create update],                                         Profile
+      cannot %i[destroy],                                                       Profile
     end
   end
 end
--- app/models/levels/doctor.rb
+++ app/models/levels/doctor.rb
@@ -3,6 +3,9 @@ module Levels
     def doctor_rules
       can %i[index read update],                                                User, id: @user.id
       cannot %i[create destroy],                                                User
+
+      can %i[index read],                                                       Profile
+      cannot %i[create update destroy],                                         Profile
     end
   end
 end

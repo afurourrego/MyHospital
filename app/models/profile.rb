55c7f5e0dc2ea36b2c87b200b953757771f0c98b
afurourrego <infoafuro@gmail.com>
2019-09-14 00:26:23 -0500

    added search to profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,21 @@
 class Profile < ApplicationRecord
 
   belongs_to :eps, class_name: 'Ep', optional: true
-  belongs_to :arl
+  belongs_to :arl, optional: true
 
 
   enum rh: {'A+': 1, 'B+': 2, 'AB+': 3, 'O+': 4, 'A-': 5, 'B-': 6, 'AB-': 7, 'O-': 8 }
 
   date_time_attribute :birthday
+
+  def self.search(profiles, params)
+
+    profiles = profiles.where(identification_card: params['identification_card']) unless params['identification_card'].blank?
+    profiles = profiles.where(full_name: params['full_name']) unless params['full_name'].blank?
+    profiles = profiles.where(email: params['email']) unless params['email'].blank?
+
+    profiles
+  end
 
   def self.eps_list
     Ep.all.pluck(:name, :id)

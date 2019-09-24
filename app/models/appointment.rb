b6275d66df13641410e7810e48d5c7f0b91e002e
afurourrego <infoafuro@gmail.com>
2019-09-23 23:13:52 -0500

    validation for conflicts dates

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -10,6 +10,8 @@ class Appointment < ApplicationRecord
   validates :patient, presence: true
   validates :date, presence: true
 
+  validate :conflict_date
+
   def self.search(appointments, params)
     appointments = where('date >= ?', Date.parse(params['date']).beginning_of_day) unless params['date'].blank?
     appointments = appointments.where('date <= ?', Date.parse(params['date']).end_of_day) unless params['date'].blank?
@@ -18,6 +20,20 @@ class Appointment < ApplicationRecord
     appointments = appointments.where(patient_id: params['patient_id']) unless params['patient_id'].blank?
 
     appointments
+  end
+
+  def conflict_date
+    dates = Appointment.all.where('date >= ?', date.beginning_of_day)
+    dates = dates.where('date <= ?', date.end_of_day)
+    dates = dates.where(doctor_id: doctor_id)
+    dates.each do |appointment|
+      next if appointment.id == id
+      if appointment.date.between?(date - 9.minutes, date + 9.minutes)
+        errors.add(:date, "conflict date with appointment date at #{appointment.date.strftime("%b %-e, %Y %I:%M %p")}")
+        return false
+      end
+    end
+    true
   end
 
 end
